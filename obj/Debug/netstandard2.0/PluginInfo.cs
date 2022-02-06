84707f7470b57fc52b4787cf8e87c21c52647cbf
oinkratpig <contact.knilax@gmail.com>
2022-02-05 19:19:47 -0600

    Added obj folder to .gitignore

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++ obj/Debug/netstandard2.0/PluginInfo.cs
@@ -2,8 +2,8 @@ namespace EasyCustomStuff
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "oinkratpig.plugins.easycustomstuff";
+        public const string PLUGIN_GUID = "EasyCustomStuff";
         public const string PLUGIN_NAME = "EasyCustomStuff";
-        public const string PLUGIN_VERSION = "0.0.1";
+        public const string PLUGIN_VERSION = "1.0.0";
     }
 }
