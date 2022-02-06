28710d960191582055fca7bed89d40ddeeee9368
Oinky <contact.knilax@gmail.com>
2022-02-05 19:20:20 -0600

    Delete obj directory

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace EasyCustomStuff
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "EasyCustomStuff";
-        public const string PLUGIN_NAME = "EasyCustomStuff";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
